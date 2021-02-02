0ed7ed6427696325dfee02f94fb1c96eb7d0415f
Roman M <maleev4you@gmail.com>
2021-02-02 14:13:06 +0300

    added css and js files

--- app/views/home_page.template.php
+++ app/views/home_page.template.php
@@ -6,9 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
 @include('assets.errors')
 @include('assets.menu')
+<script src="/js/bootstrap.js"></script>
 </body>
 </html>
